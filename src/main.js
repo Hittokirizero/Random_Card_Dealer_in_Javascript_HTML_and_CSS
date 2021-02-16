18ba6e1f370ab1c35d804c91746c15a29c6036c5
Hittokirizero <v_hbk@hotmail.com>
2021-02-16 03:00:02 +0000

    Primera carga de Github

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let numb = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+let simb = ["♠", "♣", "♥", "♦"];
+
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let z = Math.floor(Math.random() * simb.length);
+  let topx = simb[z];
+  let centerx = numb[Math.floor(Math.random() * numb.length)];
+  let bottomx = simb[z];
+  document.querySelector("#top").innerHTML = topx;
+  document.querySelector("#center").innerHTML = centerx;
+  document.querySelector("#bottom").innerHTML = bottomx;
 };
