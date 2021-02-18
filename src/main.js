d7e19bbdb6729892b5a9e5ed3b49638fb3b26959
Hittokirizero <v_hbk@hotmail.com>
2021-02-18 01:25:57 +0000

    Finalizacion de la carta

--- src/main.js
+++ src/main.js
@@ -10,10 +10,20 @@ let simb = ["♠", "♣", "♥", "♦"];
 
 window.onload = function() {
   let z = Math.floor(Math.random() * simb.length);
+  let colorx = "";
+  if (z == 3 || z == 2) {
+    colorx = "color:red;";
+  } else {
+    colorx = "color:black;";
+  }
   let topx = simb[z];
   let centerx = numb[Math.floor(Math.random() * numb.length)];
   let bottomx = simb[z];
   document.querySelector("#top").innerHTML = topx;
   document.querySelector("#center").innerHTML = centerx;
   document.querySelector("#bottom").innerHTML = bottomx;
+  const change_color1 = document.querySelector(" .change_color1");
+  change_color1.style = colorx;
+  const change_color2 = document.querySelector(" .change_color2");
+  change_color2.style = colorx + "transform: rotate(180deg);";
 };
